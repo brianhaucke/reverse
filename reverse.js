44dce9e2b59d5d4d2041076b258298733add1ada
brianhaucke <brian.haucke@gmail.com>
2019-09-09 15:57:04 -0700

    Initial Commit. Includes helloWorld array

---
+++ reverse.js
@@ -0,0 +1,14 @@
+function reverse(arr) {
+
+  var reversedArray = []
+
+  for (i = 0; i < arr.length; i++) {
+    reversedArray[i] = arr[(arr.length - 1) - i];
+  }
+  console.log(reversedArray);
+
+}
+
+var helloWorld = ['h', 'e', 'l', 'l', 'o', 'w', 'o', 'r', 'l', 'd']
+
+reverse(helloWorld);
